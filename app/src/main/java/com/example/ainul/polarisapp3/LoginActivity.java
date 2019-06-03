2674ba40037ab5e61b2df2b29fe0c37e80cf4fe5
Ainul Mardhiyyah <ainul_mardhiyyah@mymail.sutd.edu.sg>
2019-06-03 23:33:28 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/ainul/polarisapp3/LoginActivity.java
@@ -0,0 +1,148 @@
+package com.example.polaris_2019;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/*
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;*/
+
+// Login page
+
+public class LoginActivity extends AppCompatActivity {
+    //private FirebaseAuth mAuth;
+    private ProgressDialog mProgress;
+
+    private String TAG = "001";
+
+    private TextView EmailField;
+    private TextView PassField;
+
+    private Button LoginButton;
+    private Button RegisterButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        /*
+        FirebaseApp.initializeApp(LoginActivity.this);
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        Intent intent = getIntent();
+        String activity = intent.getStringExtra("from"); */
+
+        /* PERMISSIONS */
+
+        int PERMISSION_ALL = 1;
+        String[] PERMISSIONS = {
+                Manifest.permission.READ_CONTACTS,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.GET_ACCOUNTS
+        };
+
+        if(!hasPermissions(this, PERMISSIONS)){
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+
+
+        LoginButton = findViewById(R.id.LoginButton);
+        RegisterButton = findViewById(R.id.RegisterButton);
+
+        EmailField = findViewById(R.id.EmailField);
+        PassField = findViewById(R.id.PassField);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, ConnectAnotherActivity.class));
+            }
+        });
+
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        /* LOG IN */
+        /* TODO: AUTH STUFF */
+        //final FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        /*
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mEmail = EmailField.getEditableText().toString();
+                String mPass = PassField.getEditableText().toString();
+                signIn(mEmail,mPass);
+            }
+        });
+
+        RegisterButton = findViewById(R.id.RegisterButton);
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });*/
+
+    }
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /*
+    protected void signIn(String email, String password){;
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            finish();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            finish();
+//                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        }
+
+                    }
+                });
+    }
+    */
+}
